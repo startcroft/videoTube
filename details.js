9bfcf6e6f56e3c0334ff3f05b9481eb1829d5727
startcroft <santiagogomezpavas42@gmail.com>
2023-03-22 15:26:28 -0500

    Segunda Versión

--- details.js
+++ details.js
@@ -8,3 +8,48 @@ const reproductor = videos.find(item => item.id === videoId);
 
 const videoContenedor = document.querySelector(".video");
 videoContenedor.innerHTML = `${reproductor.reproduction}`
+
+const profile = document.querySelector(".chanelProfile")
+profile.innerHTML = `
+    <img class="chanel" src=${reproductor.image} >
+`
+
+const videoDetails = document.querySelector(".characteristics");
+videoDetails.innerHTML = `
+<h1 class="titleVideo">${reproductor.title}</h1>
+<div class="videoInfo">
+<p>${reproductor.views} - </p>
+<p>${reproductor.date}</p>
+</div>
+`;
+
+const PintarvideosSugeridos = (contenedor, listaVideos, id) => {
+
+    const sugeridosContainer = document.createElement("section");
+
+    sugeridosContainer.classList.add("suggested");
+
+    const videosSugeridos = listaVideos.filter((element) => element.id !== id);
+
+    videosSugeridos.forEach(element => {
+        sugeridosContainer.innerHTML += `
+        <article class="videoCard">
+        <figure class="miniaturaList" ><img class="miniaturaList" src=${element.miniatura} alt=${element.title} /></figure>
+        <section class="infoList" >
+          <h3>${element.title}</h3>
+            <div>
+             
+             <span>${element.chanel}</span>
+            </div>
+        </section>
+        </article>
+        `;
+
+        contenedor.appendChild(sugeridosContainer);
+    });
+}
+
+document.addEventListener(`DOMContentLoaded`, () =>{
+    const videoContent = document.querySelector(".sugeridos");
+    PintarvideosSugeridos(videoContent, videos, videoId);
+   });
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const printVideos = (contenedor, videos) => {
         contenedor.innerHTML += `
         <article class="videoCard">
         <div class="article__video">
-        <img class="iframe" src="${Element.miniatura}" alt="${Element.title}">
+        <img class="iframe"  src="${Element.miniatura}" alt="${Element.title}" name=${Element.id}>
            <div class="time" >${Element.duration}</div>
          </div> 
          <div class="footer">
@@ -37,11 +37,37 @@ document.addEventListener(`DOMContentLoaded`, () =>{
 })
 
 document.addEventListener(`click`, (event) => {
-    if(event.target.classList.contains("videoTitle")){
+    if(event.target.classList.contains("videoTitle") || event.target.classList.contains("iframe")){
         console.log(event.target);
 
         const id = event.target.getAttribute("name");
         sessionStorage.setItem("videoId", JSON.stringify(id));
         window.location.href="details.html";
     }
+})
+
+
+// Para filtrar por categorias
+
+const filter = ["all"];
+
+videos.forEach(element => {
+    if(!filter.includes(element.category)){
+        filter.push(element.category);
+    }
+})
+
+filter.forEach(element => {
+    const filtro = document.getElementsByName(element)[0];
+
+    filtro.addEventListener("click", () => {
+        const videosFiltrados = 
+        element === "all"
+        ? videos
+        : videos.filter(
+            (item) => item.category === element
+             );
+
+             printVideos(contenedor, videosFiltrados)
+    })
 })
